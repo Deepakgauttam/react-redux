fdf884ee4d918b34ac8fe68d00532d8189f58433
Deepakgauttam <105534881+Deepakgauttam@users.noreply.github.com>
2023-05-02 09:56:21 +0530

    create-action.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 
-
+import { addToCart } from "./redux/action";
+import { useDispatch } from "react-redux";
 
 function App() {
-  return <div>App</div>;
+  const dispatch = useDispatch();
+  const product = {
+    name: "i Phone",
+    category: "mobile",
+    price: 10000,
+    color: "red",
+  };
+  return (
+    <div className="App">
+      <button onClick={() => dispatch(addToCart(product))}>Add To Cart</button>
+    </div>
+  );
 }
 
 export default App;
